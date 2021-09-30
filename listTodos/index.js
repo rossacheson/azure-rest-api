c00444ca804564383ffacc727424e241f7da7e3a
Ross Acheson <racheson@fivestardev.com>
2021-09-30 16:46:36 -0400

    filter on username

--- listTodos/index.js
+++ listTodos/index.js
@@ -9,7 +9,7 @@ module.exports = async function (context, req) {
     const connection = await MongoClient.connect(URL);
     const todoCollection = connection.db(DATABASE_NAME).collection(COLLECTION_NAME);
 
-    const results = await todoCollection.find({}).toArray();
+    const results = await todoCollection.find({ username: "in28minutes" }).toArray();
 
     return {
         // status: 200, /* Defaults to 200 */
