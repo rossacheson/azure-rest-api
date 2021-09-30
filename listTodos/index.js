bb1eacce406ac91b5ef4cdd715c2fd5714ce2951
Ross Acheson <racheson@fivestardev.com>
2021-09-30 16:35:42 -0400

    Get todos from Cosmos/Mongo

--- listTodos/index.js
+++ listTodos/index.js
@@ -1,7 +1,18 @@
+const MongoClient = require('mongodb').MongoClient;
+
 module.exports = async function (context, req) {
+
+    const URL = 'mongodb://cosmos-db-todos-rda:uX2BF3GA2SaLQl5aZbLsBUJNO8CF9D25Hzl4nZvznZv1pbk3DK7hVVV20vFasUETzLGDw7TcVBuqlM1h8DLHNQ%3D%3D@cosmos-db-todos-rda.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-todos-rda@'; // process.env.MONGODB_URL;
+    const DATABASE_NAME = 'serverless'; // process.env.MONGODB_DATABASE_NAME;
+    const COLLECTION_NAME = 'todos'; //process.env.MONGODB_COLLECTION_NAME;
+
+    const connection = await MongoClient.connect(URL);
+    const todoCollection = connection.db(DATABASE_NAME).collection(COLLECTION_NAME);
+
+    const results = await todoCollection.find({}).toArray();
 
     return {
         // status: 200, /* Defaults to 200 */
-        body: 'dummy response'
+        body: JSON.stringify(results)
     };
 }
