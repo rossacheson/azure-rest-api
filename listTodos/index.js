dc7f4a2b4ae44a8d0027b70dc2bb03ec001f2082
Ross Acheson <racheson@fivestardev.com>
2021-09-30 16:44:21 -0400

    Get mongodb constants from local.settings.json

--- listTodos/index.js
+++ listTodos/index.js
@@ -2,9 +2,9 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports = async function (context, req) {
 
-    const URL = 'mongodb://cosmos-db-todos-rda:uX2BF3GA2SaLQl5aZbLsBUJNO8CF9D25Hzl4nZvznZv1pbk3DK7hVVV20vFasUETzLGDw7TcVBuqlM1h8DLHNQ%3D%3D@cosmos-db-todos-rda.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-db-todos-rda@'; // process.env.MONGODB_URL;
-    const DATABASE_NAME = 'serverless'; // process.env.MONGODB_DATABASE_NAME;
-    const COLLECTION_NAME = 'todos'; //process.env.MONGODB_COLLECTION_NAME;
+    const URL = process.env.MONGODB_URL;
+    const DATABASE_NAME = process.env.MONGODB_DATABASE_NAME;
+    const COLLECTION_NAME = process.env.MONGODB_COLLECTION_NAME;
 
     const connection = await MongoClient.connect(URL);
     const todoCollection = connection.db(DATABASE_NAME).collection(COLLECTION_NAME);
@@ -13,6 +13,6 @@ module.exports = async function (context, req) {
 
     return {
         // status: 200, /* Defaults to 200 */
-        body: JSON.stringify(results)
+        body: JSON.stringify(results).replace(/_id/g, "id")
     };
 }
